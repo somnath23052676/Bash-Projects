b7550442a5149901068a44ce79bc152a3ddce0b0
Somnath Sinha Mahapatra <23052676@kiit.ac.in>
2024-10-10 21:02:19 +0530

    Add files via upload

---
+++ login.bash
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+GREEN="\033[32m"
+RESET="\033[0m"
+
+login() {
+	echo -e "${GREEN}who are you${RESET}" 
+	read username 
+
+	echo -e "${GREEN}what is your password ? ${RESET} "
+	read -s password 
+
+	if [ "$username" = "somnath" ] && [ "$password"  = "Code@1" ]; then 
+		echo -e "$(figlet -f small "access granted $username ") "
+		sleep 3
+		clear
+	else 
+		echo -e " get out imposter " 
+		exit 1
+	fi
+}
+
+while ! login; do
+	exit  
+done 
+
